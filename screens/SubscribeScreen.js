1abc61ce2b7f3ab016d4bb6e25dc99b2e8657577
ljreaux <146141261+ljreaux@users.noreply.github.com>
2024-07-08 22:03:55 -0500

    finished project

--- screens/SubscribeScreen.js
+++ screens/SubscribeScreen.js
@@ -1,9 +1,81 @@
-import * as React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, Text, Image, StyleSheet, Pressable, TextInput, KeyboardAvoidingView, ScrollView } from 'react-native';
+import LittleLemonLogo from '../assets/little-lemon-logo-grey.png';
+import { validateEmail } from "../utils/index"
 
 const SubscribeScreen = () => {
-  // Add subscribe screen code here
-  return <View></View>;
+  const [email, setEmail] = useState("");
+  const isValid = validateEmail(email);
+
+  return (
+    <KeyboardAvoidingView behavior="padding" style={styles.container}
+    >
+      <ScrollView style={styles.container}>
+        <Image source={LittleLemonLogo} style={styles.image} />
+        <Text style={styles.subscribeText}>
+          Subscribe to our newsletter for our latest delicious recipes!
+        </Text>
+        <TextInput
+          value={email}
+          onChangeText={setEmail}
+          placeholder='Type your email'
+          keyboardType='email-address'
+          style={styles.inputStyles}
+        />
+        <Pressable
+          onPress={() => Alert.alert('Thanks for subscribing, stay tuned!')}
+          style={styles.buttonStyles}
+          disabled={!isValid}
+        >
+          <Text style={[styles.buttonText, !isValid && {backgroundColor: '#ccc'}]}>
+            Subscribe
+          </Text>
+        </Pressable>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
 };
 
 export default SubscribeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    width: 150,
+    height: 150,
+    resizeMode: 'contain',
+    marginTop: 100,
+    marginBottom: 20,
+    alignSelf: 'center'
+  },
+  subscribeText: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 40,
+  },
+  inputStyles: {
+    borderWidth: 1,
+    borderColor: '#3E524B',
+    padding: 10,
+    marginBottom: 20,
+    borderRadius: 8,
+    width: '80%',
+    alignSelf: 'center'
+  },
+  buttonStyles: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+    borderRadius: 8,
+    overflow: 'hidden',
+  },
+  buttonText: {
+    flex: .8,
+    textAlign: 'center',
+    fontSize: 20,
+    backgroundColor: '#3E524B',
+    color: 'white',
+    padding: 10,
+  }
+})
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,9 +1,61 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { View, Text, Image, StyleSheet, Pressable } from 'react-native';
+import LittleLemonLogo from '../assets/little-lemon-logo.png';
 
 const WelcomeScreen = ({ navigation }) => {
   // Add welcome screen code here.
-  return <View></View>;
+  return (
+  <View style={{flex: 1}}>
+    <Image source={LittleLemonLogo} style={styles.image} />
+    <Text style={styles.welcomeText}>
+      Little Lemon, your local Mediterranean Bistro
+    </Text>
+    <Pressable 
+    onPress={() => navigation.navigate('Subscribe')}
+    style={styles.buttonStyles}
+    >
+        <Text style={styles.buttonText}>
+          Newsletter
+        </Text>
+    </Pressable>
+  </View>
+  );
 };
 
 export default WelcomeScreen;
+
+
+const styles = StyleSheet.create({
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+    marginTop: 100,
+    marginBottom: 20,
+    alignSelf: 'center'
+  },
+  welcomeText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 40,
+  },
+  buttonStyles: {
+    marginTop: 'auto',
+    marginBottom: 75,
+    flexDirection: 'row',
+    alignSelf: 'center',
+    borderRadius: 8,
+    overflow: 'hidden',
+  }, 
+  buttonText: {
+    flex: .8,
+    textAlign: 'center',
+    fontSize: 20,
+    backgroundColor: '#3E524B',
+    color: 'white',
+    padding: 10,
+       
+    
+  }
+})
